19c52a9f5dcc7b09530ae355d1eb3f1042ee7433
Pradeep Verma <pradeepnetcore@outlook.com>
2025-10-16 22:44:52 +0530

    fortune_cookie

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter First Demo Home Page",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+  
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _currentFortune = "";
+  final _fortuneList = [
+    "You will have a great day!",
+    "Success is on the horizon.",
+    "Happiness is a choice.",
+    "Adventure awaits you.",
+    "Believe in yourself.",
+  ];
+
+  void _randomizeFortune() {
+    var random = Random();
+    int fortune = random.nextInt(_fortuneList.length);
+   
+    setState(() {
+      _currentFortune = _fortuneList[fortune];
+    });
+  }
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Flutter First Demo Fortune App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Your Fortune for today is:"),
+            Text(_currentFortune,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _randomizeFortune,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
